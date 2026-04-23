c379d1c4e54879253b6ce4ea2653f20c9caf154d
magnatavr <benoyevskiy@email.ru>
2026-04-23 12:38:49 +0300

    feat: add factories and seeders for categories and products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,11 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'category_id'];
+
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class);
